5ce3232e1b25979f4bb4998eedfc38dc722b2ac9
Varun Barad <varun@varunbarad.com>
2023-01-12 19:34:08 +0530

    Refactor square pattern generation to separate methods

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use std::path::Path;
-use std::str::FromStr;
-use clap::{arg, Command};
-use image::{Rgba, RgbaImage};
+use clap::{arg, ArgMatches, Command};
+use image::{ImageBuffer, Rgba, RgbaImage};
 use imageproc::drawing::draw_filled_rect_mut;
 use imageproc::rect::Rect;
 use palette::rgb::Rgb;
 use palette::{FromColor, Hsl, Srgb};
 use rand::Rng;
+use std::path::Path;
+use std::str::FromStr;
 
 fn generate_color_like(base: Srgb<f32>) -> Srgb<f32> {
     let hsl: Hsl = Hsl::from_color(base);
@@ -50,36 +50,61 @@ fn main() {
                 .arg(arg!(--width <VALUE>).required(true))
                 .arg(arg!(--height <VALUE>).required(true))
                 .arg(arg!(--size <VALUE>).required(true))
-                .arg(arg!(--"base-color" <VALUE>).required(true).default_value("#1b4332")),
+                .arg(
+                    arg!(--"base-color" <VALUE>)
+                        .required(true)
+                        .default_value("#1b4332"),
+                ),
         )
         .get_matches();
 
     match program_arguments.subcommand() {
-        Some(("square", arguments)) => {
-            let output_path = Path::new(arguments.get_one("output").unwrap() as &String);
-
-            let image_width = u32::from_str_radix(arguments.get_one("width").unwrap() as &String, 10u32).unwrap();
-            let image_height = u32::from_str_radix(arguments.get_one("height").unwrap() as &String, 10u32).unwrap();
-            let pattern_size = u32::from_str_radix(arguments.get_one("size").unwrap() as &String, 10u32).unwrap();
-
-            let base_color = Srgb::from_str(arguments.get_one("base-color").unwrap() as &String).unwrap().into_format();
-
-            let mut img = RgbaImage::new(image_width, image_height);
-
-            for i in 0..(f32::ceil((image_width as f32) / (pattern_size as f32)) as u32) {
-                for j in 0..(f32::ceil((image_height as f32) / (pattern_size as f32)) as u32) {
-                    let tile_color = Rgba::from_srgb(generate_color_like(base_color));
-                    let x: i32 = (i * pattern_size) as i32;
-                    let y: i32 = (j * pattern_size) as i32;
-                    let pattern = Rect::at(x, y).of_size(pattern_size, pattern_size);
-                    draw_filled_rect_mut(&mut img, pattern, tile_color);
-                }
-            }
-
-            img.save(output_path).unwrap();
-        }
+        Some(("square", arguments)) => handle_subcommand_square(arguments),
         _ => {
             eprintln!("No known pattern found")
         }
     }
+}
+
+fn handle_subcommand_square(arguments: &ArgMatches) {
+    let output_path = Path::new(arguments.get_one("output").unwrap() as &String);
+
+    let image_width = (arguments.get_one("width").unwrap() as &String)
+        .parse::<u32>()
+        .unwrap();
+    let image_height = (arguments.get_one("height").unwrap() as &String)
+        .parse::<u32>()
+        .unwrap();
+    let pattern_size = (arguments.get_one("size").unwrap() as &String)
+        .parse::<u32>()
+        .unwrap();
+
+    let base_color = Srgb::from_str(arguments.get_one("base-color").unwrap() as &String)
+        .unwrap()
+        .into_format();
+
+    generate_square_pattern(image_width, image_height, pattern_size, base_color)
+        .save(output_path)
+        .unwrap();
+}
+
+fn generate_square_pattern(
+    image_width: u32,
+    image_height: u32,
+    pattern_size: u32,
+    base_color: Rgb,
+) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+    let mut img = RgbaImage::new(image_width, image_height);
+
+    for i in 0..(f32::ceil((image_width as f32) / (pattern_size as f32)) as u32) {
+        for j in 0..(f32::ceil((image_height as f32) / (pattern_size as f32)) as u32) {
+            let tile_color = Rgba::from_srgb(generate_color_like(base_color));
+            let x: i32 = (i * pattern_size) as i32;
+            let y: i32 = (j * pattern_size) as i32;
+            let pattern = Rect::at(x, y).of_size(pattern_size, pattern_size);
+            draw_filled_rect_mut(&mut img, pattern, tile_color);
+        }
+    }
+
+    img
 }
